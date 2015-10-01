d9aa9f3557fadbb5aae4624c3fe8c1eb4a6c5a96
Tiffany Bogus & Jon Nardine <student@epicodus.com>
2015-09-30 20:13:01 -0700

    Add other penguins; move jquery scripts to top of html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
+$(document).ready(function() {
   $("#toggle").click(function() {
-  $(".tog").toggle();
-});
+    $(".tog").toggle();
+  });
 
- $("#fade").click(function() {
-  $(".fad").fadeToggle();
+  $("#fade").click(function() {
+    $(".fad").fadeToggle();
+  });
+
+  $("#slide").click(function() {
+    $(".sld").slideToggle();
+  });
+
+  $("#toggle2").click(function() {
+    $(".nerd").toggle();
+  });
+
+  $("#fade2").click(function() {
+    $(".hand").fadeToggle();
+  });
+
+  $("#slide2").click(function() {
+    $(".old").slideToggle();
+  });
+
+
+
+
+
 });
